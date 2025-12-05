65a181921c1b06fa59db228013612c382d2bf823
André devkcud Albanese Junior <patommmmm@proton.me>
2025-12-05 11:56:24 -0300

    fix: remove zoom in floating windows and restore zoom on WinEnter

--- lua/zoom/win.lua
+++ lua/zoom/win.lua
@@ -3,9 +3,23 @@ local M = {}
 -- TODO: Hide statusline/tabline when zoomed
 -- TODO: Add animation when zooming in/out
 -- TODO: Add support for percentage-based zooming
+-- TODO: Add option to zoom only in one direction (horizontal/vertical)
 
 -- FIXME: When switching windows (C-w h|j|k|l), the zoom state should be toggled off
+vim.api.nvim_create_autocmd("WinEnter", {
+	-- HACK: This is a possible solution for the issue, but will not be translated when we switch to independent window states eventually
+	-- FIXME: This may cause cursor flickering when switching windows (especially with smear-cursor)
+	callback = function()
+		if M.is_zoomed() then
+			M.restore()
+		end
+	end,
+})
 
+-- FIXME: Keep track of zoomed windows instead of a global state
+-- This would allow multiple windows to be zoomed independently.
+-- However, it would require more complex state management and
+-- may lead to conflicts with splits and their rules.
 local zoomed = false
 local restore_cmd = nil
 
@@ -14,7 +28,11 @@ local function is_available()
 end
 
 function M.toggle()
-	-- FIXME: If the window is floating, resizing may not work as expected
+	-- TODO: Add support for floating windows (this gonna be a pain)
+	if vim.api.nvim_win_get_config(0).relative ~= "" then
+		vim.notify("Zoom: cannot zoom floating windows", vim.log.levels.WARN)
+		return
+	end
 
 	if not is_available() then
 		vim.notify("Zoom: Not enough space to zoom", vim.log.levels.WARN)
@@ -24,6 +42,7 @@ function M.toggle()
 	if not zoomed then
 		restore_cmd = vim.fn.winrestcmd()
 
+		-- TODO: Actually "hide" other windows instead of just resizing
 		vim.cmd("resize")
 		vim.cmd("vertical resize")
 
@@ -55,6 +74,5 @@ function M.zoom()
 end
 
 -- TODO: Add integration with other plugins (e.g., auto-restore on buffer switch)
--- TODO: Add support for floating windows
 
 return M
